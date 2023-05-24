72ced13427156039d24386cf57d4ff6390169a66
omo <98173891+omoNattie@users.noreply.github.com>
2023-05-24 11:28:07 +0300

    Forgot some summaries

--- catboys-cs/Source/Http.cs
+++ catboys-cs/Source/Http.cs
@@ -2,11 +2,20 @@
 
 namespace Catboys;
 
+/// <summary>
+/// Represents the Http class used for calling the API
+/// </summary>
 public static class Http
 {
     private static readonly string _baseUrl = "https://api.catboys.com";
     private static readonly HttpClient _httpClient = new HttpClient();
 
+	/// <summary>
+	/// Calls the api.
+	/// </summary>
+	/// <param name="endpoint">The endpoint to call</param>
+	/// <returns>Dictionary composed of the HTTP response</returns>
+	/// <exception cref="NullReferenceException">If the api returns nothing.</exception>
     public static async Task<Dictionary<string, string>> GetEndpoint(string endpoint)
     {
 		// This is unorthodox
